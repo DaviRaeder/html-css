a67bb16f77122ea5c7464e65e4bacb75fc188b78
Davi Raeder <98568443+DaviRaeder@users.noreply.github.com>
2022-07-14 18:35:41 -0300

    atualizei o html

---
+++ Treino de CSS/ex001/script.js
@@ -0,0 +1,6 @@
+var hamburguer = document.querySelector('.hamburguer')
+
+function clicou() {
+    hamburguer.classList.toggle('active')
+}
+
---
+++ Treino de CSS/ex003/main.js
@@ -0,0 +1,6 @@
+var ul = document.querySelector('nav ul')
+var menuBtn = document.querySelector('.menu-btn i')
+
+function menuShow() {
+    ul.classList.toggle('open')
+}
